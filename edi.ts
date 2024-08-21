14cfdc1367e46a3e3fd567c8109f4dc186d82ca0
chakupewa <chakupewajanuary38@gamil.com>
2024-08-21 09:55:20 +0300

    marekebisho mapya

---
+++ edi.ts
@@ -0,0 +1,4 @@
+const Name=document.querySelector(".name");
+const form=document.querySelector("#form");
+console.log("why you are not included in the system");
+alert("we are working for the family and country interest to reach certain level in the developing nations")
