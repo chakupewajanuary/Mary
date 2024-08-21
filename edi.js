59f93d43e05eed623b8ff1931fb4f5b32da6bbee
chakupewa <chakupewajanuary38@gamil.com>
2024-08-21 16:31:34 +0300

    new commit

--- edi.js
+++ edi.js
@@ -49,12 +49,111 @@ const todoiscompleted= todos.filter(function(tai){
 });
 console.log(todoiscompleted);
 
-//combined filter and map
+//combined filter and map 
 const todocompleted= todos.filter(function(tai){
     return tai.iscompleted===true;
 }).map(function(tai){
     return tai.id;
 })
 console.log(todocompleted);
+
+// function
+
+const add=(num1,num2)=>num1+num2;
+console.log(add(4,4));
+//or
+function addnum(numb1,numb2){
+    return numb1+numb2;
+}
+console.log(addnum());
+todos.forEach((ty)=>console.log(ty));
+
+
+// constructive function
+function person(firstname , lastname, dob){
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.dob=dob;
+}
+//object instantate
+const person1= new person("jau","mwani","2024-03-14");
+console.log(person1);
+const {firstname,lastname}=person1
+console.log(firstname);
+
+
+//class 
+class Mtu{
+    constructor(firstname , lastname, dob){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.dob=new Date(dob);
+    }
+    getFullname(){
+        return `${this.firstname} and ${lastname}`
+    }
+
+     
+}
+const mtu1=new Mtu("mwana","good","2023-06-10")
+console.log(mtu1.getFullname());
+console.log(mtu1.dob)
+
+
+
+
+const tt=document.querySelector('#uu');
+tt.textContent="JINA";
+
+const btn=document.querySelector('.btn');
+btn.style.background="red";
+
+//works the same as bellow
+/*
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log("click");
+})
+
+// works the same as above
+btn.addEventListener('click',onClick);
+
+function onClick(e){
+    e.preventDefault();
+    console.log("click");
+    
+ }
+    */
+ const Form=document.getElementById("form");
+ const Name=document.getElementById("name");
+ const Email=document.getElementById("email");
+ const Msg=document.getElementById("msg")
+ 
+
+Form.addEventListener('submit',onSubmit)
+
+function onSubmit(e){
+    e.preventDefault();
+    if(Name.value==""||Email.value==""){
+        Msg.innerHTML="please fill the fields";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
