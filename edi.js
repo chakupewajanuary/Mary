7e1a7983bd8ddcac7ee812c6270853e89b820d1f
chakupewa <chakupewajanuary38@gamil.com>
2024-08-21 13:54:27 +0300

    fourth updates

--- edi.js
+++ edi.js
@@ -8,7 +8,7 @@ const todos=[
         iscompleted:true
     },
     {
-        id:1,
+        id:2,
         text:'meet with teacher',
         iscompleted:false
     },
@@ -29,5 +29,32 @@ for (let i = 0; i < todos.length; i++) {
 //function as above for loop
 for(let todo of todos){
     console.log(todo.id);
-
 }
+
+/*couple of array loop(forEach, map-create new array in the array,
+filter-create new array based on a condition)
+*/
+//forEach
+todos.forEach(function(tai){
+    console.log(tai.iscompleted);  
+});
+//for map
+ const todotext= todos.map(function(tai){
+    return tai.text;
+});
+console.log(todotext);
+//filter
+const todoiscompleted= todos.filter(function(tai){
+    return tai.iscompleted===true;
+});
+console.log(todoiscompleted);
+
+//combined filter and map
+const todocompleted= todos.filter(function(tai){
+    return tai.iscompleted===true;
+}).map(function(tai){
+    return tai.id;
+})
+console.log(todocompleted);
+
+
